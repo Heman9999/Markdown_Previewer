2c72df2da33cfcfe38bfb8ba00ebe4f1dfd07401
Heman9999 <hemantrao9999@gmail.com>
2023-07-27 11:14:50 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {marked} from 'marked'
+import {useState} from 'react'
 
 function App() {
+  let [value,setValue] = useState(`# Hello World
+## you are awesome
+
+[link](http://google.com)
+1. one
+1. two
+1. three
+1. four
+
+>Hello World
+![Image](https://code.visualstudio.com/assets/docs/languages/Markdown/markdown-outline-view.png)
+
+**Hello World**
+\`this \`
+
+\`\`\`
+var i;
+for(i =0; i < 5; i++){
+  console.log(i)
+}
+\`\`\`
+
+`)
+  const Hello = marked.parse(`${value}`)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='title'>
+        <h1>
+          MarkDown Previewer
+        </h1>
+      </div>
+      <div className='container'>
+        <div className='textfield'>
+          <textarea value={value} id="editor" onChange={e => setValue(e.target.value)}>
+            
+          </textarea>
+        </div>
+        <div className='showText'>
+          <div className='viwer' id="preview" dangerouslySetInnerHTML={{ __html : Hello}}>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
